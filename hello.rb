b1f89c5bb0029334af1aa8b16149a03ea51237bc
Nick Yebra <chaeyebra@gmail.com>
2018-04-05 00:17:09 +0000

    new

---
+++ hello.rb
@@ -0,0 +1,9 @@
+puts "hello world!" 
+
+gift = ["puppy", "cat", "treats", "gift card", "letter"]
+
+puts "All I got for valentine's day was #{gift.sample}."
+
+puts "my new branch"
+
+hello = "why hello there"
